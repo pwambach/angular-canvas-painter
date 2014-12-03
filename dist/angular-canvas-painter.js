a64fcdc6e728117971a179ced176f3be04a72331
Philipp Wambach <philippwambach@gmail.com>
2014-12-03 22:35:25 +0100

    new builds

--- dist/angular-canvas-painter.js
+++ dist/angular-canvas-painter.js
@@ -38,233 +38,164 @@ angular.module('pw.canvas-painter')
     return {
       restrict: 'AE',
       scope: {
-      	options: '='
+        options: '='
       },
       templateUrl: '../templates/canvas.html', 
       link: function postLink(scope, el, attrs) {
 
-      		var isTouch = 'ontouchstart' in document.documentElement;
+        var isTouch = true; // !!('ontouchstart' in window);
+        var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
 
-      		//set default options
-      		var options = scope.options
-      		options.width = options.width || 400;
-      		options.height = options.height || 300;
-      		options.backgroundColor = options.backgroundColor || '#fff';
-      		options.color = options.color || '#000';
-      		options.undoEnabled = options.undoEnabled || false;
-      		options.opacity = options.opacity || 0.9;
-      		options.lineWidth = options.lineWidth || 1;
+        var PAINT_START = isTouch ? 'touchstart' : 'mousedown';
+        var PAINT_MOVE = isTouch ? 'touchmove' : 'mousemove';
+        var PAINT_END = isTouch ? 'touchend' : 'mouseup';
 
-      		//create canvas and context
-      		var canvas = document.getElementById('pwCanvasMain');
-      		var canvasTmp = document.getElementById('pwCanvasTmp');
-      		var ctx = canvas.getContext('2d');
-      		var ctxTmp = canvasTmp.getContext('2d');
+        //set default options
+        var options = scope.options
+        options.width = options.width || 400;
+        options.height = options.height || 300;
+        options.backgroundColor = options.backgroundColor || '#fff';
+        options.color = options.color || '#000';
+        options.undoEnabled = options.undoEnabled || false;
+        options.opacity = options.opacity || 0.9;
+        options.lineWidth = options.lineWidth || 1;
 
-      		//inti variables
-      		var point = {x: 0, y: 0};
-			var ppts = [];
+        //create canvas and context
+        var canvas = document.getElementById('pwCanvasMain');
+        var canvasTmp = document.getElementById('pwCanvasTmp');
+        var ctx = canvas.getContext('2d');
+        var ctxTmp = canvasTmp.getContext('2d');
 
-			//ios check
-			var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
-			
-			//var undoImage = [];
+        //inti variables
+        var point = {x: 0, y: 0};
+        var ppts = [];
 
-	      	//set canvas size
-      		canvas.width = canvasTmp.width = options.width;
-      		canvas.height = canvasTmp.height = options.height;
+        //set canvas size
+        canvas.width = canvasTmp.width = options.width;
+        canvas.height = canvasTmp.height = options.height;
 
-      		//set context style
-	        ctx.fillStyle = options.backgroundColor;
-	        ctx.fillRect(0, 0, canvas.width, canvas.height);
-			ctxTmp.globalAlpha = options.opacity;
-			ctxTmp.lineJoin = ctxTmp.lineCap = 'round';
-			ctxTmp.lineWidth = 10;
-			ctxTmp.strokeStyle = options.color;
-			
-			
-			//Watch options
-			scope.$watch('options.lineWidth', function(newValue){
-				if(newValue && typeof newValue === 'number'){
-					ctxTmp.lineWidth = options.lineWidth = newValue;
-				}
-			});
+        //set context style
+        ctx.fillStyle = options.backgroundColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctxTmp.globalAlpha = options.opacity;
+        ctxTmp.lineJoin = ctxTmp.lineCap = 'round';
+        ctxTmp.lineWidth = 10;
+        ctxTmp.strokeStyle = options.color;
+      
+      
+        //Watch options
+        scope.$watch('options.lineWidth', function(newValue){
+          if(newValue && typeof newValue === 'number'){
+            ctxTmp.lineWidth = options.lineWidth = newValue;
+          }
+        });
 
-			scope.$watch('options.color', function(newValue){
-				if(newValue){
-					//ctx.fillStyle = newValue;	
-					ctxTmp.strokeStyle = ctxTmp.fillStyle = newValue;
-				}
-			});
+        scope.$watch('options.color', function(newValue){
+          if(newValue){
+            //ctx.fillStyle = newValue; 
+            ctxTmp.strokeStyle = ctxTmp.fillStyle = newValue;
+          }
+        });
 
-			scope.$watch('options.opacity', function(newValue){
-				if(newValue){
-					ctxTmp.globalAlpha = newValue;
-				}
-			});
-
-
-			var clearCanvas = function(){
-				ctx.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
-				ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
-			}
-
-/*
-			scope.getImageBlob = function(){
-				// Decode the dataURL    
-				var binary = atob(canvas.toDataURL().split(',')[1]);
-				// Create 8-bit unsigned array
-				var array = [];
-				for(var i = 0; i < binary.length; i++) {
-				  array.push(binary.charCodeAt(i));
-				}
-				// Return our Blob object
-				//scope.image = new Blob([new Uint8Array(array)], {type: 'image/png'});
-				return new Blob([new Uint8Array(array)], {type: 'image/png'});
-			}
-*/
-
-/*
-			scope.undo = function(){
-				if(undoImage.length > 0)
-					ctx.putImageData(undoImage.pop(), 0, 0);
-				
-				if(undoImage.length < 1)
-					scope.undoDisabled = true;
-			}
-*/
-
-			var initListeners = function(){
-				if(!isTouch){
-					//Mouse
-					window.addEventListener('mouseup', function(){
-						canvasTmp.removeEventListener('mousemove', mousePaint, false);
-						ctx.drawImage(canvasTmp, 0, 0);
-						ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
-						ppts = [];
-					}, false);
-
-					canvasTmp.addEventListener('mousedown', function(e) {
-						e.preventDefault();
-						canvasTmp.addEventListener('mousemove', mousePaint, false);
-						
-						point.x = e.offsetX !== undefined ? e.offsetX : e.layerX;
-						point.y = e.offsetY !== undefined ? e.offsetY : e.layerY;
-						ppts.push({x: point.x, y: point.y});
-						ppts.push({x: point.x, y: point.y});
-
-						//save Undo Image
-/*						undoImage.push(ctx.getImageData(0, 0, canvasTmp.width, canvasTmp.height));
-						undoImage = undoImage.slice(-10);
-						scope.$apply(function(){
-							scope.undoDisabled = false;
-						});
-*/
-						onPaint();
-					}, false);
-
-					canvasTmp.addEventListener('mouseup', function() {
-						canvasTmp.removeEventListener('mousemove', mousePaint, false);
-						ctx.drawImage(canvasTmp, 0, 0);
-						ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
-						ppts = [];
-					}, false);
-				} else {
-					//Touch
-					canvasTmp.addEventListener('touchstart', function(e) {
-						canvasTmp.addEventListener('touchmove', touchPaint, false);
-						if(!iOS) {
-							point.x = e.changedTouches[0].clientX + e.layerX;
-							point.y = e.changedTouches[0].clientY + e.layerY;
-						} else {
-							point.x = e.layerX;
-							point.y = e.layerY;
-						}
-						ppts.push({x: point.x, y: point.y});
-						ppts.push({x: point.x, y: point.y});
-
-						//save Undo Image
-/*
-						undoImage.push(ctx.getImageData(0, 0, canvasTmp.width, canvasTmp.height));
-						undoImage = undoImage.slice(-10);
-						scope.$apply(function(){
-							scope.undoDisabled = false;
-						});
-*/
-
-						//first point
-						/*
-						ctx.beginPath();
-						ctx.arc(point.x, point.y, Math.round(ctxTmp.lineWidth/2), 0, 2 * Math.PI, false);
-						ctx.fill();
-						*/
-
-						onPaint();
-					}, false);
-
-					canvasTmp.addEventListener('touchend', function() {
-						canvasTmp.removeEventListener('touchmove', touchPaint, false);
-						ctx.drawImage(canvasTmp, 0, 0);
-						ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
-						ppts = [];
-					}, false);
-				}
-			}
-
-			var mousePaint = function (e){
-				point.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
-				point.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
-				onPaint();
-			}
-
-			var touchPaint = function (e){
-				e.preventDefault();
-				if(!iOS){
-					point.x = e.changedTouches[0].clientX + e.layerX;
-					point.y = e.changedTouches[0].clientY + e.layerY;
-				} else {
-					point.x = e.layerX;
-					point.y = e.layerY;	
-				}
-				onPaint();
-			}
+        scope.$watch('options.opacity', function(newValue){
+          if(newValue){
+            ctxTmp.globalAlpha = newValue;
+          }
+        });
 
 
-			var onPaint = function (){
-				// Saving all the points in an array
-				ppts.push({x: point.x, y: point.y});
-				
-				if (ppts.length == 3) {
-					var b = ppts[0];
-					ctxTmp.beginPath();
-					ctxTmp.arc(b.x, b.y, ctxTmp.lineWidth / 2, 0, Math.PI * 2, !0);
-					ctxTmp.fill();
-					ctxTmp.closePath();
-					return;
-				}
-				
-				// Tmp canvas is always cleared up before drawing.
-				ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
+        var clearCanvas = function(){
+          ctx.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
+          ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
+        };
 
-				ctxTmp.beginPath();
-				ctxTmp.moveTo(ppts[0].x, ppts[0].y);
-				
-				for (var i = 1; i < ppts.length - 2; i++) {
-					var c = (ppts[i].x + ppts[i + 1].x) / 2;
-					var d = (ppts[i].y + ppts[i + 1].y) / 2;
-					ctxTmp.quadraticCurveTo(ppts[i].x, ppts[i].y, c, d);
-				}
-				
-				// For the last 2 points
-				ctxTmp.quadraticCurveTo(
-					ppts[i].x,
-					ppts[i].y,
-					ppts[i + 1].x,
-					ppts[i + 1].y
-				);
-				ctxTmp.stroke();
-			}
-			initListeners();
+
+        var setPointFromEvent = function(point, e) {
+          if(isTouch){
+            if(iOS){
+              point.x = e.layerX;
+              point.y = e.layerY;
+            } else {
+              point.x = e.changedTouches[0].clientX - e.target.offsetParent.offsetLeft;
+              point.y = e.changedTouches[0].clientY - e.target.offsetParent.offsetTop;  
+            }
+          } else {
+            point.x = e.offsetX !== undefined ? e.offsetX : e.layerX;
+            point.y = e.offsetY !== undefined ? e.offsetY : e.layerY;
+          }
+        };
+
+
+        var paint = function (e){
+          if(e){
+            e.preventDefault();
+            setPointFromEvent(point, e);  
+          }
+
+          // Saving all the points in an array
+          ppts.push({x: point.x, y: point.y});
+          
+          if (ppts.length == 3) {
+            var b = ppts[0];
+            ctxTmp.beginPath();
+            ctxTmp.arc(b.x, b.y, ctxTmp.lineWidth / 2, 0, Math.PI * 2, !0);
+            ctxTmp.fill();
+            ctxTmp.closePath();
+            return;
+          }
+          
+          // Tmp canvas is always cleared up before drawing.
+          ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
+
+          ctxTmp.beginPath();
+          ctxTmp.moveTo(ppts[0].x, ppts[0].y);
+          
+          for (var i = 1; i < ppts.length - 2; i++) {
+            var c = (ppts[i].x + ppts[i + 1].x) / 2;
+            var d = (ppts[i].y + ppts[i + 1].y) / 2;
+            ctxTmp.quadraticCurveTo(ppts[i].x, ppts[i].y, c, d);
+          }
+          
+          // For the last 2 points
+          ctxTmp.quadraticCurveTo(
+            ppts[i].x,
+            ppts[i].y,
+            ppts[i + 1].x,
+            ppts[i + 1].y
+          );
+          ctxTmp.stroke();
+        };
+
+
+        var initListeners = function(){
+          if(!isTouch){
+            window.addEventListener(PAINT_END, function(){
+              canvasTmp.removeEventListener(PAINT_MOVE, paint, false);
+              ctx.drawImage(canvasTmp, 0, 0);
+              ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
+              ppts = [];
+            }, false);  
+          }
+          
+          canvasTmp.addEventListener(PAINT_START, function(e) {
+            e.preventDefault();
+            canvasTmp.addEventListener(PAINT_MOVE, paint, false);
+            
+            setPointFromEvent(point, e);
+            ppts.push({x: point.x, y: point.y});
+            ppts.push({x: point.x, y: point.y});
+
+            paint();
+          }, false);
+
+          canvasTmp.addEventListener(PAINT_END, function() {
+            canvasTmp.removeEventListener(PAINT_MOVE, paint, false);
+            ctx.drawImage(canvasTmp, 0, 0);
+            ctxTmp.clearRect(0, 0, canvasTmp.width, canvasTmp.height);
+            ppts = [];
+          }, false);
+        };
+        initListeners();
       }
     };
   });
