28d016f575037697cb63c957ff8329774d78aebb
Philipp Wambach <philippwambach@gmail.com>
2014-12-06 16:37:27 +0100

    improved color selector

--- js/pwColorSelector.js
+++ js/pwColorSelector.js
@@ -4,17 +4,15 @@ angular.module('pw.canvas-painter')
   .directive('pwColorSelector', function () {
     return {
       restrict: 'AE',
-      replace: true,
       scope: {
         colorList: '=pwColorSelector',
-        color: '='
+        selectedColor: '=color'
       },
-      templateUrl: '../templates/color-selector.html', 
-      link: function postLink(scope, element, attrs) {
-        scope.select = function(index, color){
-          scope.selectedIndex = index;
-          scope.color = color;
-        };
+      templateUrl: '../templates/color-selector.html',
+      link: function(scope){
+      	scope.setColor = function(col){
+      		scope.selectedColor = col;
+      	};
       }
     };
   });
